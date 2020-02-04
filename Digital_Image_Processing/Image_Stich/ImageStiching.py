18ff996bd43932c03b151615a1f643fe9eb883c6
DarrenmondZhang <785320051@qq.com>
2020-02-04 08:12:06 +0800

    answer_sheet

---
+++ Digital_Image_Processing/Image_Stich/ImageStiching.py
@@ -0,0 +1,17 @@
+﻿from Digital_Image_Processing.Image_Stich.Stitcher import Stitcher
+from Digital_Image_Processing.basic_operation.basic_operation import *
+import cv2
+
+# 读取拼接图片
+imageA = cv2.imread("left_01.png")
+imageB = cv2.imread("right_01.png")
+
+# 把图片拼接成全景图
+stitcher = Stitcher()
+(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+
+# 显示所有图片
+cv_show("Image A", imageA)
+cv_show("Image B", imageB)
+cv_show("Keypoint Matches", vis)
+cv_show("Result", result)
